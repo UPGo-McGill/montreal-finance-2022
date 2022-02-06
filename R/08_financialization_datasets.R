25e53af00431a598951cb9e7053b5cf2dc433dd3
dwachsmuth <david.wachsmuth@mcgill.ca>
2022-02-06 10:23:43 -0500

    New script 8 to produce final datasets

---
+++ R/08_financialization_datasets.R
@@ -0,0 +1,56 @@
+#### 08 FINANCIALIZATION DATASETS ##############################################
+
+source("R/01_startup.R")
+qload("output/LL.qsm", nthreads = availableCores())
+qload("output/geometry.qsm", nthreads = availableCores())
+
+
+# Join LL data to CTs -----------------------------------------------------
+
+LL_CT <- 
+  LL_sf_centroid |> 
+  filter(number_rental_units > 0) |> 
+  st_intersection(CT) |> 
+  mutate(across(c(publicly_traded, direct_involvement_FM, financial_partners),
+                coalesce, FALSE)) |> 
+  mutate(fin = as.logical(publicly_traded + direct_involvement_FM + 
+                            financial_partners))
+
+LL_CT_summary <- 
+  LL_CT |> 
+  st_drop_geometry() |> 
+  group_by(GeoUID, fin) |> 
+  summarize(rental_units = sum(number_rental_units)) |> 
+  summarize(n_financialized = sum(rental_units[fin]),
+            total = sum(rental_units),
+            p_financialized = n_financialized / total,
+            .groups = "drop")
+
+
+# Assemble final datasets -------------------------------------------------
+
+dataset_CT <- 
+  CT |> 
+  left_join(LL_CT_summary, by = "GeoUID") |> 
+  mutate(p_financialized = coalesce(p_financialized, 0),
+         total = coalesce(total, 0)) |> 
+  relocate(geometry, .after = p_financialized)
+
+LL_CT_to_join <- 
+  dataset_CT |> 
+  st_drop_geometry() |> 
+  select(GeoUID, n_financialized:p_financialized)
+
+dataset_building <- 
+  LL_CT |> 
+  left_join(LL_CT_to_join, by = "GeoUID") |> 
+  relocate(geometry, .after = last_col())
+
+
+# Save output -------------------------------------------------------------
+
+qsavem(dataset_building, dataset_CT, file = "output/datasets.qsm", 
+       nthreads = availableCores())
+
+rm(LL_CT, LL_CT_summary, LL_CT_to_join)
+  
