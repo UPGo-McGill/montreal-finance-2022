74bcd23a4108e67783e3bd01f7af643c7e4a1a49
dwachsmuth <david.wachsmuth@mcgill.ca>
2022-02-18 08:48:14 -0500

    Geometry updates

--- R/02_geometry_import.R
+++ R/02_geometry_import.R
@@ -61,8 +61,8 @@ CT <-
                 "v_CA16_4900", "v_CA16_3957", "v_CA16_3954", "v_CA16_3411", 
                 "v_CA16_3405", "v_CA16_6698", "v_CA16_6692", "v_CA16_6725", 
                 "v_CA16_6719", "v_CA16_4896", "v_CA16_410", "v_CA16_408", 
-                "v_CA16_2397", "v_CA16_76", "v_CA16_79", "v_CA16_82", "v_CA16_244",
-                "v_CA16_1", "v_CA16_379"),
+                "v_CA16_2397", "v_CA16_76", "v_CA16_79", "v_CA16_82", 
+                "v_CA16_244", "v_CA16_1", "v_CA16_379"),
     geo_format = "sf") |> 
   st_transform(32618) |> 
   select(-c(Type, Households, `Adjusted Population (previous Census)`:CSD_UID, 
@@ -74,7 +74,8 @@ CT <-
               "mobility_one_year", "parent_mobility_one_year", 
               "mobility_five_years", "parent_mobility_five_years", 
               "five_storeys", "parent_storeys", "med_hh_income", "age_18", 
-              "age_19", "age_20_24", "age_65_plus", "age_total", "average_age", "geometry")) |> 
+              "age_19", "age_20_24", "age_65_plus", "age_total", "average_age", 
+              "geometry")) |> 
   mutate(p_condo = condo / parent_condo,
          p_renter = renter / parent_tenure, 
          p_repairs = major_repairs / parent_repairs,
@@ -113,7 +114,6 @@ CT <-
   CT_06 |> 
   select(renter) |>  
   rename(renter_06 = renter) |> 
-  mutate(renter_06 = coalesce(renter_06, 0)) |> 
   st_interpolate_aw(CT, extensive = TRUE) |> 
   st_drop_geometry() |> 
   select(renter_06) |> 
@@ -121,7 +121,8 @@ CT <-
   as_tibble() |> 
   st_as_sf() |> 
   mutate(renter = coalesce(renter, 0),
-         change_renter_dwellings = renter - renter_06, .before = geometry) |> 
+         change_renter_dwellings = renter - renter_06, .before = geometry,
+         change_renter_pct = change_renter_dwellings / dwellings) |> 
   relocate(renter_06, .after = renter)
 
 
